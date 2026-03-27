95e6a19e17f26a658b1cfaccac8b1849650e007f
lily <1079117761@qq.com>
2026-03-27 17:35:34 +0800

    feat：404页面移动端适配

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -7,29 +7,31 @@ const message = computed(() => {
 </script>
 
 <template>
-  <div class="wscn-http404-container">
-    <div class="wscn-http404 flex flex-col md:flex-row items-center justify-center">
+  <div class="wscn-http404-container flex items-center justify-center min-h-screen">
+    <div class="wscn-http404 w-full max-w-[1200px] px-4 md:px-8 flex flex-col md:flex-row items-center justify-center">
       <!-- 404 图片区域 -->
-      <div class="pic-404 relative w-full md:w-[600px] overflow-hidden">
+      <div class="pic-404 relative w-full md:w-[600px] overflow-hidden mb-8 md:mb-0">
         <img class="pic-404__parent w-full" src="@/assets/404_images/404.png" alt="404">
         <img class="pic-404__child left absolute w-20" src="@/assets/404_images/404_cloud.png" alt="404">
         <img class="pic-404__child mid absolute w-11" src="@/assets/404_images/404_cloud.png" alt="404">
         <img class="pic-404__child right absolute w-15" src="@/assets/404_images/404_cloud.png" alt="404">
       </div>
       <!-- 文字区域 -->
-      <div class="bullshit w-full md:w-[300px] py-8 md:py-0 md:pl-8 overflow-hidden">
+      <div class="bullshit w-full md:w-[300px] overflow-hidden text-center md:text-left">
         <div class="bullshit__oops text-2xl font-bold text-[#1482f0] mb-5">
           404错误!
         </div>
         <div class="bullshit__headline text-lg font-bold text-gray-800 mb-2">
           {{ message }}
         </div>
-        <div class="bullshit__info text-sm text-gray-500 mb-8">
+        <div class="bullshit__info text-sm text-gray-500 mb-8 leading-relaxed">
           对不起，您正在寻找的页面不存在。尝试检查URL的错误，然后按浏览器上的刷新按钮或尝试在我们的应用程序中找到其他内容。
         </div>
-        <router-link to="/index" class="bullshit__return-home block w-[110px] h-9 bg-[#1482f0] rounded-full text-center text-white text-sm leading-9 cursor-pointer">
-          返回首页
-        </router-link>
+        <div class="flex justify-center md:justify-start">
+          <router-link to="/index" class="bullshit__return-home block w-[110px] h-9 bg-[#1482f0] rounded-full text-center text-white text-sm leading-9 cursor-pointer">
+            返回首页
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -37,17 +39,17 @@ const message = computed(() => {
 
 <style lang="scss" scoped>
 .wscn-http404-container {
-  transform: translate(-50%, -50%);
-  position: absolute;
-  top: 40%;
-  left: 50%;
+  // 去掉绝对定位，改用 flex 居中，适配所有屏幕
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .wscn-http404 {
-  position: relative;
-  width: 1200px;
-  padding: 0 50px;
-  overflow: hidden;
+  // 最大宽度 1200px，小屏幕自动适配
+  max-width: 1200px;
+  width: 100%;
 }
 
 .pic-404 {
